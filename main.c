e7ff1c0cb7ec46d6042d4a86076d4b71c9c76646
Koray Can Yilmaz <krycnylmz@gmail.com>
2022-12-08 15:09:13 +0300

    Fix/ratations decrease and also search fun return
    left and right rotations updated with decrease integer and also search functions fixed

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 typedef struct NODE_s *NODE;
-typedef struct NODE_s{
+typedef struct NODE_s {
     NODE right;
     NODE left;
     int key;
@@ -10,18 +10,18 @@ typedef struct NODE_s{
     int height;
 } NODE_t[1];
 
-typedef struct{
+typedef struct {
     NODE root;
 } AVL_t[1], *AVL;
 
-AVL avl_init(){
-    AVL t = (AVL)malloc(sizeof(AVL_t));
+AVL avl_init() {
+    AVL t = (AVL) malloc(sizeof(AVL_t));
     t->root = NULL;
     return t;
 }
 
-NODE init_node(int key, void *data){
-    NODE node = (NODE)malloc(sizeof(NODE_t));
+NODE init_node(int key, void *data) {
+    NODE node = (NODE) malloc(sizeof(NODE_t));
     node->left = NULL;
     node->right = NULL;
     node->key = key;
@@ -31,177 +31,208 @@ NODE init_node(int key, void *data){
 }
 
 
-void tree_print(NODE node, int l){
+void tree_print(NODE node, int l) {
     int i;
-    if(node != NULL){
-        tree_print(node->right, l+1);
+    if (node != NULL) {
+        tree_print(node->right, l + 1);
         for (i = 0; i < l; ++i) {
             printf("        ");
         }
         printf("%d\n", node->key);
-        tree_print(node->left, l+1);
+        tree_print(node->left, l + 1);
     }
 }
 
-int max(int num1, int num2)
-{
-    return (num1 > num2 ) ? num1 : num2;
+int max(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
 }
 
-int get_height(NODE node){
-    if(node == NULL || node->height == NULL){
+int get_height(NODE node) {
+    if (node == NULL || node->height == NULL) {
         return 0;
-    }else{
+    } else {
         return node->height;
     }
 }
 
-NODE left_rotation(NODE parent){
+NODE left_rotation(NODE parent, int decrease) {
 
     //DONE: Decided the structure
     NODE child = parent->right;
-    parent->height = parent->height - 2;
+    parent->height = parent->height - decrease;
     NODE t2 = child->left;
 
     //DONE: Rotation
     child->left = parent;
     parent->right = t2;
     parent = child;
-    parent->height = (max(get_height(parent->left), get_height(parent->right) ) + 1);
+    parent->height = (max(get_height(parent->left), get_height(parent->right)) + 1);
 
 
     return parent;
 }
-NODE right_rotation(NODE parent){
+
+NODE right_rotation(NODE parent, int decrease) {
 
     //DONE: Decided the structure
     NODE child = parent->left;
-    parent->height = parent->height - 2;
+    parent->height = parent->height - decrease;
     NODE t2 = child->right;
 
     //DONE: Rotation
     child->right = parent;
     parent->left = t2;
     parent = child;
-    parent->height = (max(get_height(parent->left), get_height(parent->right) ) + 1);
+    parent->height = (max(get_height(parent->left), get_height(parent->right)) + 1);
 
     return parent;
 }
-NODE avl_search_rec(NODE node,int key){
-    if(node == NULL){
-        printf("Key: %d does not exist in the tree \n",key);
-    }
-    else{
-        if(key < node->key){
+
+NODE avl_search_rec(NODE node, int key) {
+    if (node == NULL) {
+        printf("Key: %d does not exist in the tree \n", key);
+    } else {
+        if (key < node->key) {
             node->left = avl_search_rec(node->left, key);
-        }
-        else if(key > node->key){
+        } else if (key > node->key) {
             node->right = avl_search_rec(node->right, key);
-        }
-        else{
-            printf("Key: %d found and height is %d !!!! Also you can see part of the tree below after key: %d \n\n",key, node->height ,key);
-            tree_print(node,0);
-            return node;
+        } else {
+            printf("Key: %d found and height is %d !!!! Also you can see part of the tree below after key: %d \n\n",
+                   key, node->height, key);
+            tree_print(node, 0);
+
         }
     }
+    return node;
 }
 
-void avl_search(AVL t, int key){
-    if(t == NULL){
+void avl_search(AVL t, int key) {
+    if (t == NULL) {
         printf("ERROR. Uninitialized tree\n");
-    }
-    else{
-        if(t->root == NULL){
+    } else {
+        if (t->root == NULL) {
             printf("There is no element in the tree \n");
-        }
-        else{
-            avl_search_rec(t->root, key);
+        } else {
+            t->root = avl_search_rec(t->root, key);
         }
     }
 }
 
-NODE avl_insert_rec(NODE node, int key, void *data){
-    if(node == NULL){
+NODE avl_insert_rec(NODE node, int key, void *data) {
+    if (node == NULL) {
         node = init_node(key, data);
-    }
-    else{
-        if(key < node->key){
+    } else {
+        if (key < node->key) {
             node->left = avl_insert_rec(node->left, key, data);
-        }
-        else if(key > node->key){
+        } else if (key > node->key) {
             node->right = avl_insert_rec(node->right, key, data);
-        }
-        else{
+        } else {
             printf("Duplicate key: %d. Ignoring...\n\n\n", key);
         }
     }
 
     //DONE: Update the height of current node;
     //DONE: node->height = max(node->left->height,node->right->height) + 1;
-    node->height = (max(get_height(node->left), get_height(node->right) ) + 1);
+    node->height = (max(get_height(node->left), get_height(node->right)) + 1);
 
     //DONE: Compute Balance Factor
     //DONE: bf = node->left->height - node->right->height
     int bf = get_height(node->left) - get_height(node->right); //Balance Factor
 
 
-    if(bf < -1){
-        if(get_height(node->right->left) < get_height(node->right->right)) {
+    if (bf < -1) {
+        if (get_height(node->right->left) < get_height(node->right->right)) {
             //PROBLEM:Right Right Problem
 
             //DONE: left Rotate
-            node = left_rotation(node);
+            node = left_rotation(node, 2);
 
-        }else{
+        } else {
             //PROBLEM:Right Left Problem
 
             //TODO: Right Rotate
-            node->right = right_rotation(node->right);
+            node->right = right_rotation(node->right, 1);
+
             //TODO: Left Rotate
-            node = left_rotation(node);
+            node = left_rotation(node, 2);
         }
-    } else if(bf > 1){
-        if(get_height(node->left->right) < get_height(node->left->left)) {
+    } else if (bf > 1) {
+        if (get_height(node->left->right) < get_height(node->left->left)) {
             //PROBLEM:Left Left Problem
 
             //TODO: Right Rotate
-            node = right_rotation(node);
-        }else{
+            node = right_rotation(node, 2);
+        } else {
             //PROBLEM:Left Right Problem
 
             //TODO: Left Rotate
-            node->left = left_rotation(node->left);
+            node->left = left_rotation(node->left, 1);
+
             //TODO: Right Rotate
-            node = right_rotation(node);
+            node = right_rotation(node, 2);
         }
     }
-
-    //TODO: CAUTION!!! node->left or node->right might be NULL;
     return node;
 }
 
-void avl_insert(AVL t, int key, void *data){
-    if(t == NULL){
+void avl_insert(AVL t, int key, void *data) {
+    if (t == NULL) {
         printf("ERROR. Uninitialized tree\n");
-    }
-    else{
-        if(t->root == NULL){
+    } else {
+        if (t->root == NULL) {
             t->root = init_node(key, data);
-        }
-        else{
+        } else {
             t->root = avl_insert_rec(t->root, key, data);
         }
     }
 }
 
+NODE avl_delete_rec(NODE node, int key) {
+    if (node == NULL) {
+        printf("The does not exist in the tree!!");
+    } else {
+        if (key < node->key) {
+            node->left = avl_delete_rec(node->left, key);
+        } else if (key > node->key) {
+            node->right = avl_delete_rec(node->right, key);
+        } else {
+            printf("You found the key\n\n\n", key);
+            // Check the node if it is leaf or not.if it is delete it
 
+/*            if (node->left == NULL && node->right == NULL) {
+                free(node);
+                return NULL;
+            }*/
+
+            // If Parent
+            // has one child
+            // has two children
+
+            return node;
+
+        }
+    }
+}
+
+
+void avl_delete(AVL t, int key) {
+    if (t == NULL) {
+        printf("ERROR. Uninitialized tree\n");
+    } else {
+        if (t->root == NULL) {
+            printf("Tree is empty!!");
+        } else {
+            t->root = avl_delete_rec(t->root, key);
+        }
+    }
+}
 
 
 /*Insert All numbers into the Tree from the array*/
-void insertTest(AVL t,int* numbers,int n){
+void insertTest(AVL t, int *numbers, int n) {
     for (int i = 0; i < n; i++) {
-        printf("Num %d added\n",*(numbers+(i*2)));
-        avl_insert(t, *(numbers+(i*2)), NULL);
+        printf("Num %d added\n", *(numbers + (i * 2)));
+        avl_insert(t, *(numbers + (i * 2)), NULL);
     }
 }
 
@@ -243,11 +274,11 @@ int main() {
     avl_insert(t1, 455, NULL);
     avl_insert(t1, 500, NULL);
     avl_insert(t1, 600, NULL);
-/*    avl_insert(t1, 316, NULL);
+    avl_insert(t1, 316, NULL);
     avl_insert(t1, 390, NULL);
     avl_insert(t1, 387, NULL);
     avl_insert(t1, 310, NULL);
-    avl_insert(t1, 300, NULL);*/
+    avl_insert(t1, 300, NULL);
 
 
 
@@ -263,10 +294,14 @@ int main() {
 
 
 
-    tree_print(t1->root,0);
+    tree_print(t1->root, 0);
+    avl_search(t1, 90);
+
+
+    tree_print(t1->root, 0);
 
     //Searching Test
-    avl_search(t1,150);
+    //avl_search(t1,150);
 
     return 0;
 }
