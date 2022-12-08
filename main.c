190b773c6d2dfedcd5b4c1b2e4c185596a97befd
Koray Can Yilmaz <krycnylmz@gmail.com>
2022-12-08 12:24:16 +0300

    insert and search part
    Avl tree init tree, init node, insert node, search into the tree, left and right rotations

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ AVL avl_init(){
     t->root = NULL;
     return t;
 }
+
 NODE init_node(int key, void *data){
     NODE node = (NODE)malloc(sizeof(NODE_t));
     node->left = NULL;
@@ -29,68 +30,24 @@ NODE init_node(int key, void *data){
     return node;
 }
 
-/*void dsw_right_rot(AVL t){
-    NODE grandParent = (NODE)t;
-    NODE parent = t->root;
 
-    while(parent != NULL){
-        NODE child = parent->left;
-        if(child != NULL){
-            parent->left = child->right;
-            child->right = parent;
-            grandParent->right = child;
-            parent = child;
+void tree_print(NODE node, int l){
+    int i;
+    if(node != NULL){
+        tree_print(node->right, l+1);
+        for (i = 0; i < l; ++i) {
+            printf("        ");
         }
-        else{
-            grandParent = parent;
-            parent = parent->right;
-
-        };
-
+        printf("%d\n", node->key);
+        tree_print(node->left, l+1);
     }
-
 }
 
-int amount_of_backbone(AVL backbone_t){
-    int count = 0;
-    if(backbone_t != NULL && backbone_t->root != NULL){
-        NODE child = backbone_t->root;
-        while(child != NULL){
-            child = child->right;
-            count++;
-        }
-    }
-    return count;
-}
-
-void left_rotation(AVL backbone_t,int n){
-    NODE grandParent = (NODE)backbone_t;
-    NODE parent = backbone_t->root;
-    int copy_of_n = n;
-    while(n != 0){
-        NODE child = parent->right;
-        if(child != NULL && n == copy_of_n){
-            parent->right = child->left;
-            child->left = parent;
-            grandParent->right = child;
-            parent = child;
-            parent = parent;
-            n--;
-        }
-        else{
-            grandParent = parent;
-            parent = parent->right;
-            copy_of_n--;
-        };
-    }
-}*/
-
-void left_rotation(NODE parent){}
-void right_rotation(NODE parent){}
 int max(int num1, int num2)
 {
     return (num1 > num2 ) ? num1 : num2;
 }
+
 int get_height(NODE node){
     if(node == NULL || node->height == NULL){
         return 0;
@@ -98,10 +55,74 @@ int get_height(NODE node){
         return node->height;
     }
 }
+
+NODE left_rotation(NODE parent){
+
+    //DONE: Decided the structure
+    NODE child = parent->right;
+    parent->height = parent->height - 2;
+    NODE t2 = child->left;
+
+    //DONE: Rotation
+    child->left = parent;
+    parent->right = t2;
+    parent = child;
+    parent->height = (max(get_height(parent->left), get_height(parent->right) ) + 1);
+
+
+    return parent;
+}
+NODE right_rotation(NODE parent){
+
+    //DONE: Decided the structure
+    NODE child = parent->left;
+    parent->height = parent->height - 2;
+    NODE t2 = child->right;
+
+    //DONE: Rotation
+    child->right = parent;
+    parent->left = t2;
+    parent = child;
+    parent->height = (max(get_height(parent->left), get_height(parent->right) ) + 1);
+
+    return parent;
+}
+NODE avl_search_rec(NODE node,int key){
+    if(node == NULL){
+        printf("Key: %d does not exist in the tree \n",key);
+    }
+    else{
+        if(key < node->key){
+            node->left = avl_search_rec(node->left, key);
+        }
+        else if(key > node->key){
+            node->right = avl_search_rec(node->right, key);
+        }
+        else{
+            printf("Key: %d found and height is %d !!!! Also you can see part of the tree below after key: %d \n\n",key, node->height ,key);
+            tree_print(node,0);
+            return node;
+        }
+    }
+}
+
+void avl_search(AVL t, int key){
+    if(t == NULL){
+        printf("ERROR. Uninitialized tree\n");
+    }
+    else{
+        if(t->root == NULL){
+            printf("There is no element in the tree \n");
+        }
+        else{
+            avl_search_rec(t->root, key);
+        }
+    }
+}
+
 NODE avl_insert_rec(NODE node, int key, void *data){
     if(node == NULL){
         node = init_node(key, data);
-
     }
     else{
         if(key < node->key){
@@ -115,27 +136,43 @@ NODE avl_insert_rec(NODE node, int key, void *data){
         }
     }
 
-    //TODO: Update the height of current node;
-    //TODO: node->height = max(node->left->height,node->right->height) + 1;
-    node->height = max(get_height(node->left), get_height(node->right)+1);
+    //DONE: Update the height of current node;
+    //DONE: node->height = max(node->left->height,node->right->height) + 1;
+    node->height = (max(get_height(node->left), get_height(node->right) ) + 1);
 
-    //TODO: Compute Balance Factor
-    //TODO: bf = node->left->height - node->right->height
-
+    //DONE: Compute Balance Factor
+    //DONE: bf = node->left->height - node->right->height
     int bf = get_height(node->left) - get_height(node->right); //Balance Factor
 
 
     if(bf < -1){
         if(get_height(node->right->left) < get_height(node->right->right)) {
-            //TODO:Right Right Problem
+            //PROBLEM:Right Right Problem
+
+            //DONE: left Rotate
+            node = left_rotation(node);
+
         }else{
-            //TODO:Right Left Problem
+            //PROBLEM:Right Left Problem
+
+            //TODO: Right Rotate
+            node->right = right_rotation(node->right);
+            //TODO: Left Rotate
+            node = left_rotation(node);
         }
     } else if(bf > 1){
         if(get_height(node->left->right) < get_height(node->left->left)) {
-            //TODO:Left Left Problem
+            //PROBLEM:Left Left Problem
+
+            //TODO: Right Rotate
+            node = right_rotation(node);
         }else{
-            //TODO:Left Right Problem
+            //PROBLEM:Left Right Problem
+
+            //TODO: Left Rotate
+            node->left = left_rotation(node->left);
+            //TODO: Right Rotate
+            node = right_rotation(node);
         }
     }
 
@@ -158,31 +195,78 @@ void avl_insert(AVL t, int key, void *data){
 }
 
 
-void tree_print(NODE node, int l){
-    int i;
-    if(node != NULL){
-        tree_print(node->right, l+1);
-        for (i = 0; i < l; ++i) {
-            printf("        ");
-        }
-        printf("%d\n", node->key);
-        tree_print(node->left, l+1);
+
+
+/*Insert All numbers into the Tree from the array*/
+void insertTest(AVL t,int* numbers,int n){
+    for (int i = 0; i < n; i++) {
+        printf("Num %d added\n",*(numbers+(i*2)));
+        avl_insert(t, *(numbers+(i*2)), NULL);
     }
 }
 
 
 int main() {
-    printf("Hello, World!\n");
 
     AVL t1 = avl_init();
-    avl_insert(t1, 40, NULL);
-    avl_insert(t1, 60, NULL);
-    avl_insert(t1, 32, NULL);
-    avl_insert(t1, 65, NULL);
-    avl_insert(t1, 43, NULL);
-    avl_insert(t1, 78, NULL);
+
+/*
+    avl_insert(t1, 100, NULL);
+    avl_insert(t1, 90, NULL);
+    avl_insert(t1, 110, NULL);
+    avl_insert(t1, 80, NULL);
+    avl_insert(t1, 85, NULL);
+*/
+
+    avl_insert(t1, 100, NULL);
+    avl_insert(t1, 150, NULL);
+    avl_insert(t1, 90, NULL);
+    avl_insert(t1, 70, NULL);
+    avl_insert(t1, 80, NULL);
+    avl_insert(t1, 50, NULL);
+    avl_insert(t1, 34, NULL);
+    avl_insert(t1, 12, NULL);
+    avl_insert(t1, 200, NULL);
+    avl_insert(t1, 124, NULL);
+    avl_insert(t1, 8, NULL);
+    avl_insert(t1, 7, NULL);
+    avl_insert(t1, 4, NULL);
+    avl_insert(t1, 58, NULL);
+    avl_insert(t1, 121, NULL);
+    avl_insert(t1, 122, NULL);
+    avl_insert(t1, 123, NULL);
+    avl_insert(t1, 125, NULL);
+    avl_insert(t1, 126, NULL);
+    avl_insert(t1, 127, NULL);
+    avl_insert(t1, 362, NULL);
+    avl_insert(t1, 397, NULL);
+    avl_insert(t1, 455, NULL);
+    avl_insert(t1, 500, NULL);
+    avl_insert(t1, 600, NULL);
+/*    avl_insert(t1, 316, NULL);
+    avl_insert(t1, 390, NULL);
+    avl_insert(t1, 387, NULL);
+    avl_insert(t1, 310, NULL);
+    avl_insert(t1, 300, NULL);*/
+
+
+
+
+
+/*    //TODO: Left Left Problem Insert Test numbers
+    int* numbersLL[7] = { 100,150,90,70,80,50,60};
+    insertTest(t1,&numbersLL,7);*/
+
+/*    //TODO: Left Right Problem Insert Test numbers
+    int* numbersLR[7] = { 50,75,40,130,45,60,30};
+    insertTest(t1,&numbersLR,7);*/
+
 
 
     tree_print(t1->root,0);
+
+    //Searching Test
+    avl_search(t1,150);
+
     return 0;
 }
